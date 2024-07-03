41367ec5f5c16b5c51fc010eef9014126b6e4e7a
Seth Caldwell <caldwellst@gmail.com>
2024-07-03 11:34:17 +0100

    Fix groups output as well

--- R/create_groups_df.R
+++ R/create_groups_df.R
@@ -9,7 +9,11 @@
 #' @noRd
 create_groups_df <- function(df) {
   if ("groups" %in% names(df)) {
-    df <- dplyr::select(df, -dplyr::any_of("areas"))
+    # extract date cols and create date df before unnesting for joining later
+    date_cols <- c("period", "from", "to", "analysis_period_start", "analysis_period_end")
+    df_dates <- dplyr::select(df, dplyr::any_of(c("analysis_id", date_cols)))
+
+    df <- dplyr::select(df, -dplyr::any_of(c("areas", date_cols)))
     df <- drop_estimate_columns(df)
 
     if (!is.data.frame(df$groups)) {
@@ -26,13 +30,37 @@ create_groups_df <- function(df) {
       )
     }
 
-    df_groups <- dplyr::rename(
-      .data = df_groups,
-      "group_id" = "id",
-      "group_name" = "name"
+    df_groups <- rename_groups_df(df_groups)
+
+    # join back with date columns after pivoting
+    df_pivoted <- pivot_population_df(
+      dplyr::select(df_groups, -dplyr::any_of(c("period", date_cols)))
     )
-    type_convert_silent(df_groups)
+
+    df_joined <- dplyr::left_join(
+      df_pivoted,
+      dplyr::distinct(df_dates),
+      by = c("analysis_id", "period")
+    )
+
+    type_convert_silent(df_joined)
   } else {
     NULL
   }
+}
+
+#' Renames the groups columns
+#'
+#' Renames columns, and makes sure that redundant columns are dropped, like
+#' `aag_id` (already contained as `group_id`) and `period`, which will be created
+#' when pivoting the estimate columns
+#'
+#' @noRd
+rename_groups_df <- function(df) {
+  df <- rename_estimate_columns(df)
+  dplyr::rename(
+    .data = df,
+    "group_id" = "id",
+    "group_name" = "name"
+  )
 }
